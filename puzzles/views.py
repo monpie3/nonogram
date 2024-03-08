0d38e4da8b3b87e88584957d1b6761efb62684c6
monpie3 <dotyk_zla@o2.pl>
2024-03-07 21:15:31 -0500

    Add navigation to base.html & Add the instruction template

--- puzzles/views.py
+++ puzzles/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "puzzles/index.html")
+
+
+def instruction(request):
+    return render(request, "puzzles/instruction.html")
