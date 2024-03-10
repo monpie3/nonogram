71e056c1e0054b6a18026c430ae28ad99899daac
monpie3 <dotyk_zla@o2.pl>
2024-03-09 22:27:59 -0500

    Move SECRET_KEY value to .env

--- nonogram/settings.py
+++ nonogram/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jl-5luj+1t0$5%971_@6au4rbdl(gn%#vz_7q4i77=5*o*mh=6"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
