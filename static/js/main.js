0a823625a9508d9786f51306fa9894b370f48f6b
monpie3 <dotyk_zla@o2.pl>
2024-03-12 21:27:17 -0400

    Add light + Don't change the color of the cube after it is dragged

--- static/js/main.js
+++ static/js/main.js
@@ -2,27 +2,32 @@ import * as THREE from 'three';
 import { DragControls } from 'https://unpkg.com/three/examples/jsm/controls/DragControls.js';
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const light = new THREE.PointLight(0xffffff, 1000)
+light.position.set(10, 10, 10)
+scene.add(light)
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshPhongMaterial({ color: 0x00ff00, transparent: true});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
 
 const controls = new DragControls([cube], camera, renderer.domElement);
 
 controls.addEventListener('dragstart', function (event) {
-    event.object.material.color.set(0xc96b65);
+    event.object.material.color.set(0x013220);
 });
 
 controls.addEventListener('dragend', function (event) {
-    event.object.material.color.set(0xf21505);
+    event.object.material.color.set(0x00ff00);
 });
 
 
@@ -34,7 +39,6 @@ function animate() {
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
-
 }
 
 animate();
