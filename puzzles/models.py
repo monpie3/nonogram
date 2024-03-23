f7e0baebb0fe32f818e42b002796655a8f6ecfbc
monpie3 <dotyk_zla@o2.pl>
2024-03-22 22:00:49 -0400

    Create draft of the Nonogram model

--- puzzles/models.py
+++ puzzles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+DIFFICULTY_CHOICES = [
+    (1, "★"),
+    (2, "★★"),
+    (3, "★★★"),
+    (4, "★★★★"),
+    (5, "★★★★★"),
+]
+
+
+class Nonogram(models.Model):
+    title = models.CharField(max_length=100)
+    puzzle_data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    difficulty_level = models.IntegerField(choices=DIFFICULTY_CHOICES)
+    author = models.CharField(max_length=100, default="Anonymous")
+
+    def __str__(self):
+        return f"{self.title} {self.difficulty_level}"
