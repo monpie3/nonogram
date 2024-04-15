8babc5a444b514129b6cd5933eb257f3a79e6284
monpie3 <dotyk_zla@o2.pl>
2024-04-14 23:15:16 -0400

    Add convert_img_to_grid func

--- puzzles/puzzle_generator.py
+++ puzzles/puzzle_generator.py
@@ -1,7 +1,13 @@
 import logging
+from enum import Enum
 
 import numpy as np
 from PIL import Image, ImageFilter
+
+
+class PixelColor(Enum):
+    WHITE = 1
+    BLACK = 0
 
 
 def erode(cycles, image):
@@ -20,6 +26,34 @@ def smooth(cycles, image):
     for _ in range(cycles):
         image = image.filter(ImageFilter.SMOOTH)
     return image
+
+
+def count_black_pixels(image_array):
+    row_counts = []
+
+    for row in image_array:
+        row_count = []
+        num_of_black = 0
+
+        for i, el in enumerate(row):
+            if el == PixelColor.WHITE.value:
+                if num_of_black > 0:
+                    row_count.append(num_of_black)
+                num_of_black = 0
+            else:
+                num_of_black += 1
+                if i == len(row) - 1:  # if last element is black
+                    row_count.append(num_of_black)
+        row_counts.append(row_count)
+    return row_counts
+
+
+def convert_img_to_grid(image):
+    image_array = np.array(image)
+    row_counts = count_black_pixels(image_array)
+    col_counts = count_black_pixels(image_array.T)
+
+    return row_counts, col_counts
 
 
 def generate_nonogram_from_image(image_path):
