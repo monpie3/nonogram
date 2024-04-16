ead24bd549bcb24b8b3edc22637ec9e3a42ae1ea
monpie3 <dotyk_zla@o2.pl>
2024-04-15 21:08:08 -0400

    Change Nonogram model - add default values

--- puzzles/models.py
+++ puzzles/models.py
@@ -13,10 +13,10 @@ class Nonogram(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to="nonogram_images/")
     puzzle_data = models.JSONField()
-    puzzle_solution = models.JSONField()
+    puzzle_solution = models.JSONField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    difficulty_level = models.IntegerField(choices=DIFFICULTY_CHOICES)
+    difficulty_level = models.IntegerField(choices=DIFFICULTY_CHOICES, default=3)
     author = models.CharField(max_length=100, default="Anonymous")
 
     def __str__(self):
