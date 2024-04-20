c6cc119e7a3688c536b71aa315ce07b5d9d9889e
monpie3 <dotyk_zla@o2.pl>
2024-04-19 21:40:10 -0400

    Add test for transform_img

--- puzzles/tests.py
+++ puzzles/tests.py
@@ -1,3 +1,35 @@
+import numpy as np
 from django.test import TestCase
 
-# Create your tests here.
+from puzzles import puzzle_generator
+
+
+class TestTransformImg(TestCase):
+    def test_transform_img(self):
+        image_array = np.array(
+            [
+                [0, 0, 0, 1, 1, 1, 0, 0],
+                [1, 0, 0, 0, 1, 1, 1, 0],
+                [0, 0, 1, 1, 0, 1, 1, 1],
+                [1, 1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 1, 1, 0, 1, 1, 1],
+                [1, 1, 1, 0, 1, 0, 0, 1],
+            ]
+        )
+
+        expected_row_counts = [[3, 2], [3, 1], [2, 1], [5], [1, 1], [1, 2]]
+        expected_col_counts = [
+            [1, 1],
+            [3, 1],
+            [2, 1],
+            [1, 1, 1],
+            [3],
+            [1, 1],
+            [1, 1, 1],
+            [2],
+        ]
+
+        row_counts, col_counts = puzzle_generator.convert_img_to_grid(image_array)
+
+        assert row_counts == expected_row_counts
+        assert col_counts == expected_col_counts
