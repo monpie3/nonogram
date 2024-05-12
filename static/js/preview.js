24fd0c59e17c94c30512f0234828fe07f3e0d425
monpie3 <dotyk_zla@o2.pl>
2024-05-11 22:54:12 -0400

    Refresh puzzle generator view - add margins

--- static/js/preview.js
+++ static/js/preview.js
@@ -4,8 +4,8 @@ function previewImage(event) {
         const reader = new FileReader();
         reader.onload = function (e) {
             const imagePreview = document.getElementById('image-preview');
-            imagePreview.innerHTML = `<img src="${e.target.result}" alt="Image Preview" style="max-width: 300px;">`;
-        }
+            imagePreview.innerHTML = `<img src="${e.target.result}" alt="Image Preview" style="max-height: 20rem;">`;
+        };
         reader.readAsDataURL(input.files[0]);
     }
 }
